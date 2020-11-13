f21088ca49dc96840a294bd5a52621a9b81b8363
Carlos Gonzalez <carlossgv@gmail.com>
2020-11-12 22:35:50 -0300

    Changed display font

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-// Variables
+/* VARIABLES */
 let firstOperand = "";
 let secondOperand = "";
 let operator = "";
@@ -8,20 +8,7 @@ let isDot = false;
 document.getElementById("display").innerHTML = "0";
 
 
-
-// Populates the display with numbers (clears screen if its after operand or zero)
-function populate(number) {
-    if ( isOperandClicked ) {
-            document.getElementById("display").innerHTML = "";
-            document.getElementById("display").innerHTML += number
-            isOperandClicked = false;
-    }
-    else {
-        document.getElementById("display").innerHTML += number;
-        isOperandClicked = false;
-    }
-}
-
+/* ASSIGMENT OF EVERY CLICK AND KEYBOARD BIND */
 
 // Asign click and keypress properties to number
 for (let i = 0; i <= 9; i++) {
@@ -49,6 +36,65 @@ document.addEventListener("keydown", (e) => {
         backSpace();
     }
 });
+
+// Asign click and keypress properties to equal button
+document.getElementById("equal").addEventListener("click", () => { equal() });
+document.addEventListener("keydown", (e) => {
+    if (e.keyCode === 13) {
+        equal();
+    }
+});
+
+// Asign click and keypress properties to clear button
+document.getElementById('clear').addEventListener("click", () => clear() );
+document.addEventListener("keydown", (e) => {
+    if (e.keyCode === 46) {
+        clear();
+    }
+});
+
+// Asign click and keypress properties to operators 
+document.getElementById("+").addEventListener("click", () => operatorFunc("+"));
+document.addEventListener("keydown", (e) => {
+    if (e.keyCode === 107) {
+        operatorFunc("+");
+    }
+});
+document.getElementById("-").addEventListener("click", () => operatorFunc("-"));
+document.addEventListener("keydown", (e) => {
+    if (e.keyCode === 109) {
+        operatorFunc("-");
+    }
+});
+document.getElementById("*").addEventListener("click", () => operatorFunc("*"));
+document.addEventListener("keydown", (e) => {
+    if (e.keyCode === 106) {
+        operatorFunc("*");
+    }
+});
+document.getElementById("/").addEventListener("click", () => operatorFunc("/"));
+document.addEventListener("keydown", (e) => {
+    if (e.keyCode === 111) {
+        operatorFunc("/");
+    }
+});
+
+
+
+/* FUNCTIONS */
+
+// Populates the display with numbers (clears screen if its after operand or zero)
+function populate(number) {
+    if ( isOperandClicked ) {
+            document.getElementById("display").innerHTML = "";
+            document.getElementById("display").innerHTML += number
+            isOperandClicked = false;
+    }
+    else {
+        document.getElementById("display").innerHTML += number;
+        isOperandClicked = false;
+    }
+}
 
 
 // Backspace function
@@ -94,49 +140,17 @@ function equal() {
         secondOperand = document.getElementById("display").innerHTML;
         answer = operate(firstOperand, secondOperand, operator);
         document.getElementById("display").innerHTML = answer;
+
         firstOperand = document.getElementById("display").innerHTML;
         isOperandClicked = true;
         firstOperand = "";
-        console.log(firstOperand,operator, secondOperand, answer);
         isDot = false;
+
+        console.log(firstOperand, operator, secondOperand, answer);
+        
     }
 }
 
-
-// Equal button and keypress behavior
-document.getElementById("equal").addEventListener("click", () => { equal() });
-document.addEventListener("keydown", (e) => {
-    if (e.keyCode === 13) {
-        equal();
-    }
-});
-
-
-// Running operators
-document.getElementById("+").addEventListener("click", () => operatorFunc("+"));
-document.addEventListener("keydown", (e) => {
-    if (e.keyCode === 107) {
-        operatorFunc("+");
-    }
-});
-document.getElementById("-").addEventListener("click", () => operatorFunc("-"));
-document.addEventListener("keydown", (e) => {
-    if (e.keyCode === 109) {
-        operatorFunc("-");
-    }
-});
-document.getElementById("*").addEventListener("click", () => operatorFunc("*"));
-document.addEventListener("keydown", (e) => {
-    if (e.keyCode === 106) {
-        operatorFunc("*");
-    }
-});
-document.getElementById("/").addEventListener("click", () => operatorFunc("/"));
-document.addEventListener("keydown", (e) => {
-    if (e.keyCode === 111) {
-        operatorFunc("/");
-    }
-});
 
 // Clear function
 function clear() {
@@ -148,17 +162,8 @@ function clear() {
     isOperandClicked = true;
 }
 
-// Clear button
-document.getElementById('clear').addEventListener("click", () => clear() );
-document.addEventListener("keydown", (e) => {
-    if (e.keyCode === 46) {
-        clear();
-    }
-});
 
-
-
-
+// Operator functions
 function add(a,b) {
     return a + b;
 }
